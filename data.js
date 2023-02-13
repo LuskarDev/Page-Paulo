1c79cbaa4394b0a38204b956e2457e82c593f209
Lucas Oliveira <87589466+LuskarDev@users.noreply.github.com>
2023-02-12 22:58:41 -0300

    comentary

---
+++ data.js
@@ -0,0 +1,6 @@
+const Cometary = [
+    {nome: "Carla", city: "São gonçalo", img: "img/stefan-stefancik-QXevDflbl8A-unsplash.jpg", coment: 'As instalações elétricas são limpas e eficientes, Otimo Eletricista muito atencioso aos detalhes em todos os aspectos.'},
+    {nome: "Antonio", city: "Rio de Janeiro", img: "img/sergio-de-paula-c_GmwfHBDzk-unsplash.jpg", coment: 'eletricista é altamente profissional, tratando cada trabalho com o maior respeito e cuidado. Recomendo'},
+    {nome: "Felipe", city: "Rio de Janeiro", img: "img/jonas-kakaroto-mjRwhvqEC0U-unsplash.jpg", coment: 'Atendimento excepcional, sempre indo além das expectativas para garantir que o cliente fique satisfeito com o trabalho. Otimo Eletricista'},
+    {nome: "Marcela", city: "Araruama", img: "img/christopher-campbell-rDEOVtE7vOs-unsplash.jpg", coment: 'O trabalho é de alta qualidade e supera todas as expectativas.'},
+]
