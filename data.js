0ad954685031f5d2b383471039100b94a501a3b5
Lucas Oliveira <87589466+LuskarDev@users.noreply.github.com>
2023-02-13 23:59:38 -0300

    adicionando novos dados

--- data.js
+++ data.js
@@ -4,3 +4,10 @@ const Cometary = [
     {nome: "Felipe", city: "Rio de Janeiro", img: "img/jonas-kakaroto-mjRwhvqEC0U-unsplash.jpg", coment: 'Atendimento excepcional, sempre indo além das expectativas para garantir que o cliente fique satisfeito com o trabalho. Otimo Eletricista'},
     {nome: "Marcela", city: "Araruama", img: "img/christopher-campbell-rDEOVtE7vOs-unsplash.jpg", coment: 'O trabalho é de alta qualidade e supera todas as expectativas.'},
 ]
+
+const service = [
+    {img: "img/repair.png", title: "Reparos Eletricos", desc: "Incluem conserto de tomadas e interruptores defeituosos, fiação e circuitos e substituir luminárias."},
+    {img: "img/casa.png", title: "Instalação Eletrica", desc: "Instalação de fiação e circuitos, montagem e conexão de equipamentos e dispositivos elétricos e comissionamento de sistemas elétricos."},
+    {img: "img/people.png", title: "Avaliação de Segurança Elétrica", desc: "Envolve uma avaliação abrangente de seu sistema elétrico para identificar possíveis riscos de segurança. Durante a avaliação, será feito uma inspecção na sua fiação, circuitos, tomadas e equipamentos elétricos para garantir que estejam tudo corretamente."},
+    {img: "img/switch.png ", title: "Atualização do Quadro de Distribuição", desc: "A atualização do quadro elétrico envolve a substituição de um quadro antigo ou desatualizado por um novo e moderno que atenda aos padrões de segurança atuais e possa lidar com as demandas elétricas de sua casa ou empresa."},
+]
